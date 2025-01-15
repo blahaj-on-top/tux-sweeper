2f75ade592a1d079f110d008a1e1c707ef0a41f6
fabian <fabianvanacoley@gmail.com>
2025-01-15 21:42:30 +0100

    grid

---
+++ grid.py
@@ -0,0 +1,15 @@
+import pygame
+
+#vars
+screen_width = 600
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+WHITE = pygame.Color(255, 255, 255)
+
+#draw grid
+def drawGrid():
+    blockSize = 50 #Set the size of the grid block
+    for x in range(0, screen_width, blockSize):
+        for y in range(0, screen_height, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(screen, WHITE, rect, 1)
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pygame
+import grid
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('TUX-SWEEPER')
 clock = pygame.time.Clock()
 pygame.display.set_icon(pygame.image.load('assets/icon.png'))
 running = True
 
+# some more vars
+WHITE = pygame.Color(255, 255, 255)
+mousepress = pygame.mouse.get_pressed()
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
+        grid.drawGrid()
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            print(x, y)
+
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    
 
     # RENDER YOUR GAME HERE
 
